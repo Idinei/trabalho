42a1bd31e50b550a424b0568e591abda0fd8e69b
Idinei <Idinei@HP-Idinei>
2014-10-22 20:30:09 -0200

    Alteração Caminho css e config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/homebank';
+$config['base_url']	= 'http://127.0.0.1/homebankxx';
 
 /*
 |--------------------------------------------------------------------------
